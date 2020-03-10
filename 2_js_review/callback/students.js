f4671ded7a7c63cc4173674ad4752342d7e83b4c
Zsolt Toth <tothzs87@gmail.com>
2020-03-10 12:40:57 +0100

    callback example
    Array processing task is solved.

--- 2_js_review/callback/students.js
+++ 2_js_review/callback/students.js
@@ -8,10 +8,25 @@ const students = [
 console.log(students);
 
 //Average Score
+console.log(students.reduce(
+    (total,current)=>{return total + current.score},0) / students.length);
 
 //Passed Students
-
+console.log(students.filter((student)=>{return student.score >= 50.0}));
 //Print Results Table
 //Alice has failed!
 //Bob has passed!
 //etc.
+var students_extended = students.map((student)=>{
+    if(student.score >= 50.0){
+        console.log("%s has passed!", student.name);
+    }
+    else{
+        console.log("%s has failed!", student.name);
+    }
+    student['hasPassed'] = student.score >= 50.0;
+    return student;
+});
+console.log(students_extended);
+
+
