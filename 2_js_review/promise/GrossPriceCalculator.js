b191cac58c829bfd4a29744ce575579ac9884639
Zsolt Toth <tothzs87@gmail.com>
2020-03-10 13:01:39 +0100

    promise example
    GrossPriceCalculator.js
    task has been added.

--- 2_js_review/promise/GrossPriceCalculator.js
+++ 2_js_review/promise/GrossPriceCalculator.js
@@ -1,5 +1,13 @@
 class GrossPriceCalculator{
 
+    constructor(){
+        this.vatRate = 27.0;
+    }
+
+    //Callculate the total gross price then return with the result.
+    totalGrossPrice_ret(products){
+        return 0;
+    }
 }
 
 module.exports = new GrossPriceCalculator();
