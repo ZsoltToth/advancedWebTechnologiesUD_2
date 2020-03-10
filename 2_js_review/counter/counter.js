846eccd25428d346b35c4c771a71545197d27ae9
Zsolt Toth <tothzs87@gmail.com>
2020-03-10 12:11:17 +0100

    counter example
    naive implementation is added

---
+++ 2_js_review/counter/counter.js
@@ -0,0 +1,9 @@
+var count = 0;
+var increase = function(){
+  count++;
+};
+
+module.exports = {
+    count : count,
+    increase : increase
+}
---
+++ 2_js_review/counter/main.js
@@ -0,0 +1,5 @@
+const counter = require('./counter');
+
+console.log(counter.count);
+counter.increase();
+console.log(counter.count);
