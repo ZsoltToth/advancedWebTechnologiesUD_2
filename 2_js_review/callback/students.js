0e2650c192ef30bb205ebdd1c8415088160b4cb0
Zsolt Toth <tothzs87@gmail.com>
2020-03-10 12:22:56 +0100

    callback example
    Task is given with students array.

---
+++ 2_js_review/callback/students.js
@@ -0,0 +1,14 @@
+const students = [
+    {name: "Alice", score: 42},
+    {name: "Bob", score:74},
+    {name: "Charlie", score: 90},
+    {name: "Denis", score: 39}
+];
+
+console.log(students);
+
+//Average Score
+
+//Passed Students
+
+//Print Results Table
