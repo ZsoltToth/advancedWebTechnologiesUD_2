e8125ec727cd675d29ff3c91a08c5896cb9f5493
Zsolt Toth <tothzs87@gmail.com>
2020-03-24 12:26:07 +0100

    A single book can be generated

--- 3_faker/bookFaker.js
+++ 3_faker/bookFaker.js
@@ -1,3 +1,34 @@
 const faker = require('faker');
 
-console.log(`${faker.name.firstName()} ${faker.name.lastName()}`);
+//console.log(`${faker.name.firstName()} ${faker.name.lastName()}`);
+
+generateAuthor = () =>{
+    return {
+        name : `${faker.name.firstName()} ${faker.name.lastName()}`,
+        nationality : faker.address.country()
+    };
+};
+var authors = [];
+for(let i = 0; i < 100; i++){
+    author = generateAuthor();
+    author.id = i;
+    authors.push(author);
+}
+//console.log(authors);
+
+generateBook = (id) => {
+    titleLength = Math.round(Math.random() * 3) + 1;
+    authorCnt = Math.round(Math.random() * 2) + 1;
+    bookAuthors = [];
+    console.log(authorCnt);
+    for(let i = 0; i < authorCnt; i++){
+        bookAuthors.push(faker.random.arrayElement(authors));
+    }
+    return {
+        id : id,
+        title : faker.lorem.words(titleLength),
+        teaser: faker.lorem.paragraph(),
+        authors : bookAuthors
+    };
+};
+console.log(generateBook());
