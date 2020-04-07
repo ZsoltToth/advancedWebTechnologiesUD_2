fb215ca3f53742804733e81528029fe3af043dd6
Zsolt Toth <tothzs87@gmail.com>
2020-04-07 12:20:58 +0200

    React State Example
    From is created.

--- 5_react_state/src/components/ToDoList.js
+++ 5_react_state/src/components/ToDoList.js
@@ -5,7 +5,36 @@ class ToDoList extends React.Component{
     render(){
         return(
             <div>
-                <h1>ToDoList</h1>
+                <div className="row">
+                    <table>
+                        <tr>
+                            <td>Name</td>
+                            <td>
+                                <input type="text"/>
+                            </td>
+                        </tr>
+                        <tbody>
+                        <tr>
+                            <td>Description</td>
+                            <td>
+                                <input type="text"/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Estimation</td>
+                            <td>
+                                <input type="number"/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan={2}>
+                                <button className="btn btn-info">Add</button>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className="row"></div>
             </div>
         );
     }
