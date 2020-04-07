5b09df86b2d4aa296f21e84ee6e3a24a97daf057
Zsolt Toth <tothzs87@gmail.com>
2020-04-07 12:12:32 +0200

    React State Example
    Initial commit

---
+++ 5_react_state/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="container-fluid">
+        <div className="row">
+            <div className="col-md-2"></div>
+            <div className="col-md-8">
+                <h1>Todo List</h1>
+            </div>
+            <div className="col-md-2"></div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
