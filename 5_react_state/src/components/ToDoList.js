ff9c73a3eafacfc7b1619ab69bcddc0e1d94265d
Zsolt Toth <tothzs87@gmail.com>
2020-04-07 12:29:40 +0200

    React State Example
    Form can be validated.

--- 5_react_state/src/components/ToDoList.js
+++ 5_react_state/src/components/ToDoList.js
@@ -2,33 +2,76 @@ import React from 'react';
 
 class ToDoList extends React.Component{
 
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            form : {
+                name : 'Wash Clothes',
+                description : 'We have clothes',
+                estimation : 1.5
+            },
+            todos : []
+        };
+    }
+
     render(){
         return(
             <div>
                 <div className="row">
                     <table>
+                        <tbody>
                         <tr>
                             <td>Name</td>
                             <td>
-                                <input type="text"/>
+                                <input
+                                    type="text"
+                                    value={this.state.form.name}
+                                    onChange={(e)=>{
+                                        let st = this.state;
+                                        st.form.name = e.target.value;
+                                        this.setState(st);
+                                    }}
+                                />
                             </td>
                         </tr>
-                        <tbody>
                         <tr>
                             <td>Description</td>
                             <td>
-                                <input type="text"/>
+                                <input
+                                    type="text"
+                                    value={this.state.form.description}
+                                    onChange={(e)=>{
+                                        let st = this.state;
+                                        st.form.description = e.target.value;
+                                        this.setState(st);
+                                    }}
+                                />
                             </td>
                         </tr>
                         <tr>
                             <td>Estimation</td>
                             <td>
-                                <input type="number"/>
+                                <input
+                                    type="number"
+                                    value={this.state.form.estimation}
+                                    onChange={(e)=>{
+                                        let st = this.state;
+                                        let est = parseFloat(e.target.value);
+                                        st.form.estimation = est > 0.0 ? est : 0.0;
+                                        this.setState(st);
+                                    }}
+                                />
                             </td>
                         </tr>
                         <tr>
                             <td colSpan={2}>
-                                <button className="btn btn-info">Add</button>
+                                <button
+                                    className="btn btn-info"
+                                    onClick={()=>{
+                                        console.log(this.state.form);
+                                    }}
+                                >Add</button>
                             </td>
                         </tr>
                         </tbody>
