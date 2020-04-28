5c9328d00780e5fadf201df28f319e541e5a01eb
Zsolt Toth <tothzs87@gmail.com>
2020-04-28 12:48:41 +0200

    ShoppingCart implemented and tested.

--- 8_testing/ShoppingCart.js
+++ 8_testing/ShoppingCart.js
@@ -7,11 +7,27 @@ class ShoppingCart {
     }
 
     addItem({name,quantity, price}){
+        const itemIndex = this._items.findIndex((item) =>{return item.name === name});
+        if(itemIndex === -1) {
+            this._items.push(
+                {
+                    name: name,
+                    quantity: quantity,
+                    price: price
+                }
+            );
+        }
+        else{
+            this._items[itemIndex].quantity += quantity;
+        }
 
     }
 
     totalPrice(){
-        return 0;
+        return this._items.reduce((total,current)=>{
+            return total +
+                current.price * current.quantity;
+        },0);
     }
 }
 
